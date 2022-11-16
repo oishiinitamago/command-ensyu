fe0db522ca029c70aee45d3b9566d276e96f16ac
k.mogi <k.mogi.sys22@morijyobi.ac.jp>
2022-11-16 15:06:24 +0900

    間違いを修正

--- Main.java
+++ Main.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
                 Scanner stdIn = new Scanner(System.in);
 
-                System.out.print("身長(m)を教えてください:");
-                double height = stdIn.nextDouble();
-                System.out.print("体重(kg)を教えてください:");
-                double weight = stdIn.nextDouble();
+ 		System.out.print("1人目の身長(cm):");
+                double height1 = stdIn.nextDouble();
+                System.out.print("1人目の体重(kg):");
+                double weight1 = stdIn.nextDouble();
+		double h1 = height1 / 100;
+                double bmi1 = weight1 / h1 / h1;
+        	
+        	System.out.print("2人目の身長(cm):");
+                double height2 = stdIn.nextDouble();
+                System.out.print("2人目の体重(kg):");
+                double weight2 = stdIn.nextDouble();
+		double h2 = height2 / 100;
+                double bmi2 = weight2 / h2 / h2;
 
-                double bmi = weight / (Math.pow(height, 2) * 100) * 100;
-
-                System.out.println("BMI値は"+ (Math.floor(bmi * 100)) / 100);
+                System.out.println("1人目のBMI:"+ (Math.floor(bmi1 * 100)) / 100);
+	        System.out.println("2人目のBMI:"+ (Math.floor(bmi2 * 100)) / 100);
         }
 }
-
